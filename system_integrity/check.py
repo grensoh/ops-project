6dd3685d90119c1bf89daaf43cf6713055283284
grensoh <95960964+grensoh@users.noreply.github.com>
2025-03-30 17:33:51 +0200

    Update check.py

--- system_integrity/check.py
+++ system_integrity/check.py
@@ -1,5 +1,8 @@
 import gc
-import machine
+from machine import Pin
+import time
+
+led = Pin(25, Pin.OUT)
 
 gc.collect()
 
@@ -8,3 +11,9 @@ mem_free = gc.mem_free()
 
 print(f"Mémoire allouée : {mem_allocated} bytes")
 print(f"Mémoire libre : {mem_free} bytes")
+
+while True:
+    led.value(1) # turn on led
+    time.sleep(1) # wait 1 second
+    led.value(0) # turn off led
+    time.sleep(1) # wait 1 second
