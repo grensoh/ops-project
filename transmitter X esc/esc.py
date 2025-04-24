68097c2a43f91ca7cfd199ca93c9010ca25bb04d
grensoh <95960964+grensoh@users.noreply.github.com>
2025-04-25 01:31:21 +0200

    Update esc.py

--- transmitter X esc/esc.py
+++ transmitter X esc/esc.py
@@ -1,48 +1,16 @@
+from machine import Pin
 import time
-from machine import Pin, PWM
 
-pwm_pin = PWM(Pin(1))  # pin used to control esc
-pwm_pin.freq(50)       # PWM signal frequency (50 Hz)
+pin_horaire = Pin(2, Pin.OUT)
+pin_antihoraire = Pin(4, Pin.OUT)
 
-def set_speed(speed):
-    """Sets speed between -100 and 100 (%)"""
-
-    # Accomodate center zone (for reverse, start at 1460 us)
-    if speed > 0.0:
-        middle = 1500000
-    else:
-        middle = 1460000
-
-    range = 500000.0
-    ns = middle + int((speed/100.0) * range)
-    #print(f"us={ns/1000}")
-    pwm_pin.duty_ns(ns)
-
-def calibrate():
-    print("Calibrating ESC")
-    #set_duty_cycle(pwm_pin, 2.5)  # 3% duty cycle
-    # Force ESC calibration by starting at 100%, go to -100% so ESC sees full range of input
-    for i in range(100, -100, -1):
-        set_speed(i)
-        time.sleep(0.005)
-    # Then back to 0% to complete arming
-    for i in range(-100, 0, +1):
-        set_speed(i)
-        time.sleep(0.005)
-    set_speed(0)
-    time.sleep(1)
-    print("Calibration DONE")
-
-def arm():
-    print("Arming ESC")
-    for i in range(0, 25, +1):
-        set_speed(i)
-        time.sleep(0.005)
-    # Then back to 0% to complete arming
-    for i in range(25, 0, -1):
-        set_speed(i)
-        time.sleep(0.005)
-    set_speed(0)
-    time.sleep(1)
-    #time.sleep(5)
-    print("Arming DONE")
+def set_motor(direction):
+    if direction == 1:
+        pin_antihoraire.value(0)
+        pin_horaire.value(1)
+    elif direction == -1:
+        pin_horaire.value(0)
+        pin_antihoraire.value(1)
+    elif direction == 0:
+        pin_horaire.value(0)
+        pin_antihoraire.value(0)
